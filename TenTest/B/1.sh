ab1b9e8e4b0d88d9b056ee49c9e85613777be756
rvdmtr <42989591+rvdmtr@users.noreply.github.com>
2019-10-18 10:41:56 +0300

    Update 1.sh

--- TenTest/B/1.sh
+++ TenTest/B/1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Где встречается Tensor">_result.txt;
+echo "Где встречается word">_result.txt;
 #1.1.1
-#grep Tensor ./* >>_result.txt
+#grep word ./* >>_result.txt
 echo "В каких строках встречается слово: ">>_result.txt;
 #1.1.2
-grep -n Tensor ./* >> _result.txt 2>/dev/null;
+grep -n word ./* >> _result.txt 2>/dev/null;
 echo "Сколько вхождений и в каком файле: ">>_result.txt;
 #1.1.3
-grep -c  Tensor ./* >> _result.txt 2>/dev/null;
+grep -c  word ./* >> _result.txt 2>/dev/null;
 
